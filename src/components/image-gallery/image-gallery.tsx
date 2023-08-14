d1979147208e31eb225744a8039125653a0a9318
Yogesh Singh <yogeshsinghs607@gmail.com>
2023-08-15 01:00:23 +0530

    add photos in gallery

--- src/components/image-gallery/image-gallery.tsx
+++ src/components/image-gallery/image-gallery.tsx
@@ -7,12 +7,23 @@ import img8 from "../../assets/images/img8.jpg";
 import img9 from "../../assets/images/img9.jpg";
 import img10 from "../../assets/images/img10.jpg";
 import img11 from "../../assets/images/img11.jpg";
+import img17 from "../../assets/images/img17.jpg";
+import img18 from "../../assets/images/img18.jpg";
+import img19 from "../../assets/images/img19.jpg";
+import img20 from "../../assets/images/img20.jpg";
+import img21 from "../../assets/images/img21.jpg";
+import img22 from "../../assets/images/img22.jpg";
+import img23 from "../../assets/images/img23.jpg";
+import img24 from "../../assets/images/img24.jpg";
+import img25 from "../../assets/images/img25.jpg";
+import img26 from "../../assets/images/img26.jpg";
+import img27 from "../../assets/images/img27.jpg";
 
 export const ImageGallery = () => {
     const [selectedImage, setSelectedImage] = React.useState(null);
 
     const images = [
-      img11,img10,img9,img8,img2, img4,img3,
+      img11,img10,img9,img8,img2, img4,img3, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27,
       ];
   
     const handleImageClick = (image: any) => {
